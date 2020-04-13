fa9eb482f2810cd4d6289768c3e5bcfd2cd12805
bradleygichuru <prz03@protonmail.com>
2020-04-14 01:18:44 +0300

    added the cell width argument to the constructor for saftey when calcutating neighnours

--- cell.js
+++ cell.js
@@ -1,47 +1,50 @@
 class Cell{
-  constructor(x,y,state){
+  constructor(x,y,cell_width,state){
     this.x = x;
     this.y = y;
-    this.state = state;
+    this.state = 0;
+    this.census;
+    this.neighbours;
+    this.cell_width = cell_width
   }
   update(cells){
     //filter for this cell neighbouring cells
-    let neighbours = cells.filter((el)=>{
+    this.neighbours = cells.filter((el)=>{
       return(
-        ((el.x==this.x-cell_width_)&&(el.y==this.y-cell_width_))||//
-        ((el.x==this.x)&&(el.y==this.y+cell_width_))||
-        ((el.x==this.x+cell_width_)&&(el.y-cell_width_))||
-        ((el.x==this.x-cell_width_)&&(el.y==this.y))||
-        ((el.x==this.x+cell_width_)&&(el.y==this.y))||
-        ((el.x==this.x-cell_width_)&&(el.y==this.y-cell_width_))||
-        ((el.x==this.x)&&(el.y==this.y-cell_width_))||
-        ((el.x==this.x+cell_width_)&&(el.y==this.y-cell_width_))
+        (el.x==this.x-this.cell_width && el.y==this.y-this.cell_width)||
+        (el.x==this.x && el.y==this.y+this.cell_width)||
+        (el.x==this.x+this.cell_width && el.y-this.cell_width)||
+        (el.x==this.x-this.cell_width && el.y==this.y)||
+        (el.x==this.x+this.cell_width && el.y==this.y)||
+        (el.x==this.x-this.cell_width && el.y==this.y-this.cell_width)||
+        (el.x==this.x && el.y==this.y-this.cell_width)||
+        (el.x==this.x+this.cell_width && el.y==this.y-this.cell_width)
       )
     })
     //to store stats about neighbours
-    let census = {
+    this.census = {
       neighbours_alive:0
     }
     //make census before update
-    neighbours.forEach((cell)=>{
-      if (cell.state === 1) {
-        census.neighbours_alive++;
+    this.neighbours.forEach((cell)=>{
+      if(cell.state === 1) {
+        this.census.neighbours_alive++;
       }
     })
     //update based on cellected census
-    if(this.state === 0 && census.neighbours_alive === 3){
+    if(this.state === 0 && this.census.neighbours_alive === 3){
       this.state = 1;
       //born
     }
-    else if(this.state == 1 &&( census.neighbours_alive === 2 || census.neighbours_alive ===3)){
+    else if(this.state == 1 &&( this.census.neighbours_alive === 2 || this.census.neighbours_alive ===3)){
       this.state =1;
       //lives to the next generation
     }
-    else if(census.neighbours_alive > 3){
+    else if(this.census.neighbours_alive > 3){
       this.state=0;
       //death by overcrowding
     }
-    else if(census.neighbours_alive < 2){
+    else if(this.census.neighbours_alive < 2){
       this.state = 0;
       //death by underpopulation
     }
