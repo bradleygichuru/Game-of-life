f143d154cb5934f03ba33422394c96849cf4eb77
bradleygichuru <prz03@protonmail.com>
2020-04-14 01:19:28 +0300

    modified variables to work well with pre-existing objects

--- runner_.js
+++ runner_.js
@@ -1,17 +1,22 @@
-const cell_width_ = 30;
-const grid_width = 1000;
+const cell_width_ = 20;
+const grid_width = 600;
 const grid_height = 600;
 
-grid_1 = new Grid(grid_width - 1, grid_height - 1,cell_width_);
+grid_1 = new Grid(grid_width, grid_height,cell_width_);
 grid_1.initialize_grid()
 function setup(){
   createCanvas(grid_width,grid_height);
+  frameRate(1)
 }
 
 function draw(){
   grid_1.cells.forEach((cell)=>{
     fill(51);
-    rect(cell.x,cell.y,grid_1.draw_width,grid_1.draw_width)
+    if (cell.state === 1) {
+      rect(cell.x,cell.y,grid_1.draw_width,grid_1.draw_width)
+    }
+    //rect(cell.x,cell.y,grid_1.draw_width,grid_1.draw_width)
+    //// TODO:  <14-04-20, yourname> fix cell.update//
     //grid_1.update()
   })
   //grid_1.update()
